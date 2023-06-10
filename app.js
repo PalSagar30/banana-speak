a47e2398c63081e71415e333ac602b7b88918b65
PalSagar30 <sagarpalsp3008@gmail.com>
2023-06-10 20:16:39 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("this script works")
+var btnTranslate = document.querySelector('#btn-translate');
+var txtInput = document.querySelector('#txt-input');
+
+// console.log(txtInput)
+// btnTranslate.addEventListener("click",()=>{
+//     console.log("hi");
+// })
+// btnTranslate.addEventListener("click",function x(){
+//     console.log("hi");
+// })
+
+function x(){
+    console.log("hi");
+    console.log("input",txtInput.value);
+}
+
+btnTranslate.addEventListener('click', x);
