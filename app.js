ea979e3127ad77fffae41f871b60e5ab320b1791
PalSagar30 <sagarpalsp3008@gmail.com>
2023-06-12 12:04:13 +0530

    main work

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
 var output = document.querySelector('#output');
-// console.log(txtInput)
-// btnTranslate.addEventListener("click",()=>{
-//     console.log("hi");
-// })
-// btnTranslate.addEventListener("click",function x(){
+//These 3 are same
+//1. btnTranslate.addEventListener("click",()=>{
 //     console.log("hi");
 // })
 
-function x(){
-    output.innerHTML="jskdljsakdj" + txtInput.value;
+//2. btnTranslate.addEventListener("click",function x(){
+//     console.log("hi");
+// })
+
+var serverUrl = "https://api.funtranslations.com/translate/pirate.json";
+ 
+function getTranslationUrl(text){
+    return serverUrl + "?" + "text=" + text
+}
+//3.
+ function x(){
+    //output.innerHTML="jskdljsakdj" + txtInput.value;
+
+    var inputTxt = txtInput.value; //taking input
+
+    //calling server from processing
+    fetch(getTranslationUrl(inputTxt))
+    .then(response=> response.json())
+    .then(json =>{
+        var translatetxt =json.contents.translated;
+        output.innerText= translatetxt;
+    })
 }
 
 btnTranslate.addEventListener('click', x);
+
